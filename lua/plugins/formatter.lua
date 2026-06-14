824ac6e68b25f23656b3e660b71142b8a320aa4d
Pranav Raja <saipranav64@gmail.com>
2026-06-14 17:02:57 +1000

    edited formatter.lua to adjust space length for new lines for clang and prettier

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,32 +1,47 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      local conform = require("conform")
-      
-      conform.setup({
-        formatters_by_ft = {
-          python = { "black" },
-          c = { "clang-format" },
-          cpp = { "clang-format" },
-          dart = { "dart_format" },
-        },
-        -- Automatically format when you save the file!
-        format_on_save = {
-          lsp_fallback = true,
-          timeout_ms = 500,
-        },
-      })
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		opts = {
+			formatters_by_ft = {
+				python = { "black" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				dart = { "dart_format" },
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+			},
+			format_on_save = {
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+			-- Custom overrides for your specific formatters!
+			formatters = {
+				["clang-format"] = {
+					-- Pass the 4-space indent rule directly to the command line execution
+					prepend_args = { "-style={BasedOnStyle: Google, IndentWidth: 4}" },
+				},
+				["prettier"] = {
+					-- Tell prettier to drop the 2-space rule and use 4
+					prepend_args = { "--tab-width", "4" },
+				},
+			},
+		},
+		config = function(_, opts)
+			local conform = require("conform")
+			conform.setup(opts)
 
-      -- Shortcut: Space + f + m to manually format
-      vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-        conform.format({
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 500,
-        })
-      end)
-    end,
-  }
+			vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+				conform.format({
+					lsp_format = "fallback",
+					async = false,
+					timeout_ms = 500,
+				})
+			end, { desc = "Format current buffer" })
+		end,
+	},
 }
-
